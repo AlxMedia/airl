81705a7f60cd180450e58c1ea63dfa08a395a660
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-01-27 12:39:33 +0100

    front widgets styling

--- inc/front-widgets-bottom.php
+++ inc/front-widgets-bottom.php
@@ -1,5 +1,5 @@
 <?php if ( is_home() && !is_paged() && get_theme_mod('frontpage-widgets-bottom') == 'on' ): ?>
-	<div class="front-widgets light-widgets group">
+	<div class="front-widgets group">
 
 		<div class="front-widget-col">
 			<div class="front-widget-inner">
--- inc/front-widgets-top.php
+++ inc/front-widgets-top.php
@@ -1,5 +1,5 @@
 <?php if ( is_home() && !is_paged() && get_theme_mod('frontpage-widgets-top') == 'on' ): ?>
-	<div class="front-widgets light-widgets group">
+	<div class="front-widgets group">
 
 		<div class="front-widget-col">
 			<div class="front-widget-inner">
